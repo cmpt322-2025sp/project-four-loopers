d742e8c4ff9dc54207f81798839e0801e3fd5ed5
Elizabeth Carmichael <ecc1225@westminsteru.edu>
2025-02-25 13:31:50 -0700

    Created database classes

--- frognition/playScene/models.py
+++ frognition/playScene/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class AdditionProblem(models.Model):
+    question = models.CharField(max_length=15)
+    answer = models.IntegerField()
+    wrongs = models.TextField()
+
+class SubtractionProblem(models.Model):
+    question = models.CharField(max_length=15)
+    answer = models.IntegerField()
+    wrongs = models.TextField()
+
+class DigitProblem(models.Model):
+    question = models.CharField(max_length=30)
+    answer = models.IntegerField()
+    wrongs = models.TextField()
