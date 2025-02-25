25c95594e74ad33daa6c091c5ca645230e4edc13
eb1113 <eb1113@westminsteru.edu>
2025-02-25 13:52:23 -0700

    added these changes not good

--- frognition/playScene/models.py
+++ frognition/playScene/models.py
@@ -1,4 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
+class React(models.Model):
+    question = models.CharField(max_length=200)
+    answer = models.CharField(max_length=100)
--- frognition/settings.py
+++ frognition/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'playScene',
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -48,7 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny']}
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'frognition.urls'
 
