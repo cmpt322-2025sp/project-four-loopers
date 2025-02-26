dc189aac722f6eb5b5d1b09b4368383427471839
Elizabeth Carmichael <ecc1225@westminsteru.edu>
2025-02-26 11:13:23 -0700

    Fixing IDE errors

--- frognition/playScene/serializers.py
+++ frognition/playScene/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Problem, Fly, Player, Attempt
+from .models import Problem, Fly
 
 class ProblemSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,13 +11,13 @@ class FlySerializer(serializers.ModelSerializer):
         model = Fly
         fields = '__all__'
 
-class PlayerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Player
-        fields = '__all__'
-
-class AttemptSerializer(serializers.ModelSerializer): 
-    class Meta:
-        model = Attempt
-        fields = '__all__'
-        
+# class PlayerSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Player
+#         fields = '__all__'
+#
+# class AttemptSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Attempt
+#         fields = '__all__'
+#
