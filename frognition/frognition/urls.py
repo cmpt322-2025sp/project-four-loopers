9aa14b1b4dea8fec6ab3df0f1f464d77542045be
eb1113 <eb1113@westminsteru.edu>
2025-02-26 13:55:06 -0700

    added the * in urls.py

--- frognition/frognition/urls.py
+++ frognition/frognition/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
-from playScene.views import
+from playScene.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
