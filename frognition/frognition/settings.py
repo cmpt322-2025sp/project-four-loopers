cb17175b22b58fc57e1d057a127d6692d1624865
eb1113 <eb1113@westminsteru.edu>
2025-02-26 11:55:43 -0700

    added settings.py

--- frognition/frognition/settings.py
+++ frognition/frognition/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "playScene",
+    "rest_framework",
+    "corsheaders",
+
 ]
 
 MIDDLEWARE = [
@@ -47,7 +51,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
+REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny']}
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'frognition.urls'
 
