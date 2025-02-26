891e4963111d4e571be9a23f50fbe1a57174e75a
eb1113 <eb1113@westminsteru.edu>
2025-02-26 11:04:38 -0700

    changed models.py

--- frognition/playScene/models.py
+++ frognition/playScene/models.py
@@ -2,25 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Problem(models.Model):
-    opperand1 = models.IntegerField()
-    opperand2 = models.IntegerField()
-    correct_answer = models.IntegerField()
+    num1 = models.IntegerField()
+    num2 = models.IntegerField()
+    correct_answer = models.ForeignKey('Fly', on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.opperand1} + {self.opperand2} = ?"
+        return f"{self.num1} + {self.num2} = ?"
 
 class Fly(models.Model):
-    problem = models.ForeignKey(Problem, on_delete=models.CASCADE,realeted_name='flies')
-    number_on_wings = models.IntegerField()
-    is_correct = models.BooleanField()
+    flyForProblem = models.ManyToManyField(Problem) # This is a many-to-many relationship with problem and fly
+    number = models.IntegerField()
 
-class Player(models.Model):
-    name = models.CharField(max_length=100)
-    score = models.IntegerField()
+# class Player(models.Model):
+#     name = models.CharField(max_length=100)
+#     score = models.IntegerField()
 
-class Attempt(models.Model):
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)
-    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
-    selected_fly = models.ForeignKey(Fly, on_delete=models.CASCADE)
-    is_correct = models.BooleanField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+# class Attempt(models.Model):
+#     player = models.ForeignKey(Player, on_delete=models.CASCADE)
+#     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+#     is_correct = models.BooleanField()
+#     timestamp = models.DateTimeField(auto_now_add=True)
