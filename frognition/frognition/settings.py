690c08a37f603317c0e9209525711e9ceb0f68f1
Elizabeth Carmichael <ecc1225@westminsteru.edu>
2025-03-04 16:45:57 -0700

    DB should be okay now

--- frognition/frognition/settings.py
+++ frognition/frognition/settings.py
@@ -87,18 +87,18 @@ WSGI_APPLICATION = 'frognition.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.mysql',
-    #    'NAME': 'MathApp_dev',
-    #    'USER': 'frog-dev',
-    #    'PASSWORD': 'dev-passwd',
-    #    'HOST': '192.168.1.11',
-    #    'PORT': '3306',
-    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MathApp_dev',
+        'USER': 'frog-dev',
+        'PASSWORD': 'dev-passwd',
+        'HOST': '192.168.1.11',
+        'PORT': '3306',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
