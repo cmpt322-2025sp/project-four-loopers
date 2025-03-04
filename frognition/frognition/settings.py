6e10662641d66cd1cb749d2a2b853ea18c35fac2
Elizabeth Carmichael <ecc1225@westminsteru.edu>
2025-03-04 16:13:36 -0700

    Reverted to SQLite to debug internal communication

--- frognition/frognition/settings.py
+++ frognition/frognition/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from tutorial.settings import BASE_DIR
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-ytg8krar(copd7sr$ck1k-6z829s^y)n*!!)hioavj4a!rwu_+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django.stargazer-vega.ts.net']
+ALLOWED_HOSTS = ['django.stargazer-vega.ts.net', '127.0.0.1']
 
 
 # Application definition
@@ -85,13 +87,17 @@ WSGI_APPLICATION = 'frognition.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.mysql',
+    #    'NAME': 'MathApp_dev',
+    #    'USER': 'frog-dev',
+    #    'PASSWORD': 'dev-passwd',
+    #    'HOST': '192.168.1.11',
+    #    'PORT': '3306',
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'MathApp_dev',
-        'USER': 'frog-dev',
-        'PASSWORD': 'dev-passwd',
-        'HOST': '192.168.1.11',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
