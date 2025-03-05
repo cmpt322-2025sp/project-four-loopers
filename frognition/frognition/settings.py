8ac62bc36b950b3d6538d47453571d154fc03f10
Elizabeth Carmichael <ecc1225@westminsteru.edu>
2025-03-04 17:39:24 -0700

    Okay, let's try this

--- frognition/frognition/settings.py
+++ frognition/frognition/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-from tutorial.settings import BASE_DIR
+# from tutorial.settings import BASE_DIR
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
