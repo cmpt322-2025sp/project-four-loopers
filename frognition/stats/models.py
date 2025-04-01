c69e4055ffdc3b8b21198d6d61381a1da7693d6f
Elizabeth Carmichael <liz.c.carmichael@gmail.com>
2025-04-01 15:20:50 -0600

    Formalized choices for problem types

--- frognition/stats/models.py
+++ frognition/stats/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Attempt(models.Model):
+    class ProblemTypes(models.TextChoices):
+        ADDITION = 'AD', _('Addition')
+        SUBTRACTION = 'SU', _('Subtraction')
+        PLACE_VALUE = 'PV', _('Place Value')
+        RANDOM = 'RA', _('Random')
+
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    problem_type = models.CharField(max_length=50)  # 'addition', 'subtraction', 'place_value', or 'random'
+    problem_type = models.CharField(max_length=2, choices=ProblemTypes.choices, default=ProblemTypes.ADDITION)
     correct = models.IntegerField()
     total = models.IntegerField()
     timestamp = models.DateTimeField(auto_now_add=True)
