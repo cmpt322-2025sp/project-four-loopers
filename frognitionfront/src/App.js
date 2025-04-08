a4df154bf9fb399b7390a74d909aee5e9b3fa29f
prashunaryal <pa0222@westminsteru.edu>
2025-04-08 12:37:14 -0600

    modified timer

--- frognitionfront/src/App.js
+++ frognitionfront/src/App.js
@@ -2,11 +2,12 @@ import './addlevel.css';
 import React from 'react';
 import GameLevel from './GameLevel';
 import AdditionLevel from './AdditionLevel';
+import StartScreen from './startScreen';
 
 function App() {
   return (
     <div className="app">
-      <AdditionLevel />
+      <AdditionLevel/>
 </div>
     
   );
